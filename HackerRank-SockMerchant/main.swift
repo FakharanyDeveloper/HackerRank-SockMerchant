99326d80b82afa3b5719b4f0fe979ff8de561115
khaled Elfakharany <khaled.elfakharany@catchsmart.com>
2019-01-13 16:03:05 +0200

    Initial Commit

---
+++ HackerRank-SockMerchant/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  HackerRank-SockMerchant
+//
+//  Created by khaled Elfakharany on 13/01/2019.
+//  Copyright © 2019 khaled Elfakharany. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
